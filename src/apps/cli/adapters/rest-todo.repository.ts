6652f78b7611936181d11f0f1a28a71e89c051b6
Alexander <alex@spotlab.org>
2021-04-04 16:54:19 +0200

    refactor: make cli rest-repository adapter independant from frameworks or libs

--- src/apps/cli/adapters/rest-todo.repository.ts
+++ src/apps/cli/adapters/rest-todo.repository.ts
@@ -1,30 +1,43 @@
 import Repository from "../../../libs/todos/use-cases/repository";
 import Todo from "../../../libs/todos/entities/todo.entity";
 
-import { ClientRequest, RequestOptions, IncomingMessage } from "http";
+export interface Request {
+    body?: string;
+    headers?: any;
+    method: string;
+    host: string;
+    port: number;
+    path: string;
+}
 
-// TODO: externalise so this adapter does not depend on frameworks and libraries
-const httpRequest = (options: RequestOptions): Promise<IncomingMessage> => new Promise((resolve, reject) => {
-    const req = new ClientRequest(options);
-    req.on("response", (res: IncomingMessage) => resolve(res))
-    req.on("error", (error: Error) => reject(error))
-    req.end()
-})
+export interface Response {
+    statusCode?: number;
+    body?: string;
+    headers: any;
+}
+
+export interface HTTPClient {
+    request: (options: Request) => Promise<Response>;
+}
 
 export default class RESTRepository implements Repository<Todo> {
+
+    constructor(private httpClient: HTTPClient) {}
 
     async find(query: any): Promise<Todo[]> {
         const queryParams = Object.entries(query).map(e => e.join('=')).join('&')
 
-        const req = await httpRequest({
-            host: process.env.HOST,
-            port: process.env.PORT,
+        const options: Request = {
+            host: String(process.env.HOST),
+            port: Number(process.env.PORT),
             path: `/todos?${queryParams}`,
             method: "GET"
-        });
+        }
 
-        const response = JSON.parse(req.read().toString());
+        const response = await this.httpClient.request(options);
 
-        return response.data.map((i: any) => new Todo(i.id));
+        const body = JSON.parse(response.body!);
+
+        return body.data.map((i: any) => new Todo(i.id));
     }
 }
--- src/apps/cli/frameworks/todo.command.ts
+++ src/apps/cli/frameworks/todo.command.ts
@@ -4,8 +4,10 @@ import TodoService from "../../../libs/todos/use-cases/interactor";
 import RESTRepository from "../adapters/rest-todo.repository";
 import CLITodoController from "../adapters/cli-todo.controller";
 import ConsolePresenter from "../adapters/console.presenter";
+import NodeHTTPClient from "./node-http-client";
 
-const repository = new RESTRepository()
+const httpClient = new NodeHTTPClient()
+const repository = new RESTRepository(httpClient)
 const logger = new ConsolePresenter()
 const useCase = new TodoService(logger, repository);
 const controller = new CLITodoController(useCase);
