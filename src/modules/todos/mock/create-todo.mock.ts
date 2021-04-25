97a9f3cab4e315f7f355b8d168ffb167dad33bc5
Alexander <alex@spotlab.org>
2021-04-25 13:04:06 +0200

    chore: delete unused mocks

--- src/modules/todos/mock/create-todo.mock.ts
+++
@@ -1,7 +0,0 @@
-import { CreateTodo, CreateTodoRequest, CreateTodoResponse } from "../boundry/create-todo";
-
-export default class CreateTodoMock implements CreateTodo {
-    execute(request: CreateTodoRequest): Promise<CreateTodoResponse> {
-        throw new Error("Method not implemented.");
-    }
-}
--- src/modules/todos/mock/list-todos.mock.ts
+++
@@ -1,7 +0,0 @@
-import { ListTodos, ListTodosRequest, ListTodosResponse } from "../boundry/list-todos";
-
-export default class ListTodosMock implements ListTodos {
-    execute(input: ListTodosRequest): Promise<ListTodosResponse> {
-        throw new Error("Method not implemented.");
-    }
-}
