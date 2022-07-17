602a3a47f2b98a44666f3f55e643454c8fa25fd4
Alexander <alex@spotlab.org>
2022-07-17 11:18:21 +0200

    fix: dependency issues and security

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,4 +1,4 @@
-import Express, { Response } from "express";
+import Express, { Request, Response } from "express";
 
 import { config } from "dotenv";
 
@@ -16,7 +16,7 @@ API.use(morgan("combined"));
 API.use("/lists", listRouter)
 API.use("/lists/:name/todos", todoRouter)
 
-API.use("*", (err: Error, _1: any, res: Response, _2: any) => {
+API.use("*", (err: Error, _1: Request, res: Response, _2: any) => {
     res.status(500).json({ error: err.message });
     console.debug(err.stack);
 })
